4f28299ae2c08f208f476a027a1869f372befb58
idealclover <idealclover@live.com>
2022-07-13 23:01:32 +0800

    ☠️ trick way to fix a bug
    wait for https://github.com/flutter/flutter/pull/104405

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import './Utils/ThemeUtil.dart';
 void main() async {
   // var database = MyDatabase();
   await InitUtil.initBeforeStart();
+  await Future.delayed(const Duration(milliseconds: 200));
   runApp(const MyApp());
 }
 
